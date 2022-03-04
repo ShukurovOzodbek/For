af5a234aecf3ffb02ac0a3ae206877303a3f37c8
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2022-03-04 18:59:30 +0500

    for

---
+++ script.js
@@ -0,0 +1,11 @@
+// cycle
+for (let i = 1 ; i <= 5; i++){
+        document.write(`${('*'.repeat(i))}  <br>`)
+}
+
+// 2 
+for(let j = 1; j <= 5; j++ ) {
+    document.write(`
+        ${ String(j) + String(j) + String(j) }
+    `)
+} 
