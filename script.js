9ee11b9ad2f17a58482e7ce874848a6025f13de9
Ozodbek_dev <99534081+ShukurovOzodbek@users.noreply.github.com>
2022-03-05 08:56:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 for (let i = 1 ; i <= 5; i++){
         document.write(`${('*'.repeat(i))}  <br>`)
 }
-
+for (let i = 5; i >= 1; i--) {
+  let s = '';
+  
+  for (let j = 0; j < i; j++) {
+    s += '*';
+  }
+  
+  document.write(`${s}<br>`);
+}
 // 2 
 for(let j = 1; j <= 5; j++ ) {
     document.write(`
